35d0b29c2d37f4a94acedf3f06925d3e721e787f
Stanley Lio <stanleylio@gmail.com>
2017-10-02 09:43:37 +0000

    terminate when connection to xchg is dropped

--- listening.py
+++ listening.py
@@ -13,12 +13,17 @@ from pika import exceptions
 from pika.adapters import twisted_connection
 from twisted.internet import defer,reactor,protocol,task
 from twisted.web import xmlrpc,server
+from twisted.internet.defer import setDebugging
+#from twisted.python import log
 from config.config_support import Config
 from cred import cred
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logging.getLogger('pika').setLevel(logging.WARNING)
+
+setDebugging(True)
+#log.startLogging(sys.stdout)
 
 
 nodeid = socket.gethostname()
@@ -27,10 +32,13 @@ queue_name = 'to_' + nodeid
 routing_key = nodeid + '.c'
 user,passwd = nodeid,cred['rabbitmq']
 config_file = expanduser('~/config.db')
-
-
 config = Config(config_file)
 
+
+def duh(e):
+    logging.exception(e)
+    reactor.stop()
+    #sys.exit()
 
 @defer.inlineCallbacks
 def run(connection):
@@ -42,16 +50,17 @@ def run(connection):
         yield channel.basic_qos(prefetch_count=5)
         queue_object,consumer_tag = yield channel.basic_consume(queue=queue_name,no_ack=False)
         l = task.LoopingCall(read,queue_object)
-        l.start(0.01)
+        d = l.start(0.01)
+        d.addErrback(duh)
     except:
         logging.exception('Cannot connect to local C&C exchange')
         sys.exit()
 
 @defer.inlineCallbacks
 def read(queue_object):
-    ch,method,properties,body = yield queue_object.get()
-    if body:
-        try:
+    try:
+        ch,method,properties,body = yield queue_object.get()
+        if body:
             logging.debug(body)
             #d = json.loads(body.strip())
             d = json.loads(body.decode("utf-8","strict"))
@@ -59,7 +68,7 @@ def read(queue_object):
             if d.get('node',None) != nodeid:
                 logging.debug('no recipient, or message not for me. ignore')
             elif 'a' not in d or 'd' not in d:
-                logging.warning('missing action and/or data')
+                logging.warning('missing action and/or data. ignore')
             elif d.get('a',None) != 'set':
                 logging.debug('not \'set\'. ignore')
             else:
@@ -76,11 +85,16 @@ def read(queue_object):
                 
             #ch.basic_ack(delivery_tag=method.delivery_tag)
             yield ch.basic_ack(delivery_tag=method.delivery_tag)
-        except:
-            traceback.print_exc()
+    except pika.exceptions.ChannelClosed:
+        # but how to create a new channel here?
+        traceback.print_exc()
+        raise
+    except:
+        traceback.print_exc()
+        raise
+    
 
-
-class P(xmlrpc.XMLRPC):
+'''class P(xmlrpc.XMLRPC):
     def xmlrpc_get_config(self,variable_name):
         try:
             v = config.get(variable_name)
@@ -88,16 +102,20 @@ class P(xmlrpc.XMLRPC):
             return v
         except:
             logging.warning('Using default for {}'.format(variable_name))
-            return 1    # safe defaults? where to store them? boundaries? TODO TODO TODO
+            return 1    # safe defaults? where to store them? boundaries? TODO TODO TODO'''
     
 
 credentials = pika.PlainCredentials(user,passwd)
 parameters = pika.ConnectionParameters(credentials=credentials)
 cc = protocol.ClientCreator(reactor,twisted_connection.TwistedProtocolConnection,parameters)
+
 d = cc.connectTCP('localhost',5672)
 d.addCallback(lambda protocol: protocol.ready)
 d.addCallback(run)
-reactor.listenTCP(8000,server.Site(P()))
+d.addErrback(duh)
+
+#reactor.listenTCP(8000,server.Site(P()))
+
 logging.info(__file__ + ' is ready')
 reactor.run()
 
